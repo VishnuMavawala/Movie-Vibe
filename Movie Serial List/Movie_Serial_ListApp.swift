d96c4965c78f6b111f1e646b02dd6b973fdab12a
Vishnu <vishnu.mavawala@neosoftmail.com>
2026-03-26 15:02:05 +0530

    Initial Commit

---
+++ Movie Serial List/Movie_Serial_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_Serial_ListApp.swift
+//  Movie Serial List
+//
+//  Created by Neosoft on 26/03/26.
+//
+
+import SwiftUI
+
+@main
+struct Movie_Serial_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
